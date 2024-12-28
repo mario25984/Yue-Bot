89d12440424d494f20ce2e7f59c25c1a3820da62
mario25984 <marioreynaldo7473@gmail.com>
2024-12-28 15:09:57 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -80,12 +80,12 @@ global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(fa
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.ch = {
-ch1: '120363229122990393@newsletter',
-ch2: '120363319941046209@newsletter',
-ch3: '120363358176207268@newsletter',
-ch4: '120363205895430548@newsletter',
+ch1: '120363379312047212@newsletter',
+ch2: '120363300847170506@newsletter',
+ch3: '120363301683324915@newsletter',
+ch4: '120363301972046069@newsletter',
 }
-global.channelid = '120363229122990393@newsletter'
+global.channelid = '120363188791250733@newsletter'
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.cheerio = cheerio
